57afb671bc411c17990a0164b34ac55ac2e47213
gpl2210 <73338163+gpl2210@users.noreply.github.com>
2023-12-17 18:38:14 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,19 +1,43 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PhantomGopal is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC20("phantomGopal", "PGT")
-        Ownable(initialOwner)
-    {
-        _mint(msg.sender, 10 * 10 ** decimals());
+contract phantomGopal is ERC20, Ownable(msg.sender) {
+    // Constructor
+    constructor() ERC20("phantomGopal", "PGT") {
+        _mint(msg.sender, 10 * 10 ** decimals()); 
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    // Modifier to make sure that only the contract owner can call this particular function
+    modifier onlyOwnerOrApproved() {
+        require(msg.sender == owner() || msg.sender == address(this), "You are an imposter");
+        _;
+    }
+
+   
+    // function _mint(address account, uint256 amount) external onlyOwnerOrApproved {
+    //   require(account != address(0), "ERC20: mint to the zero address");
+
+    //   _totalSupply += amount;
+    //   _balances[account] += amount;
+    //   emit Transfer(address(0), account, amount);
+    // }
+
+
+    // (only contract owner)
+    function mint(address to, uint256 amount) external onlyOwnerOrApproved {
         _mint(to, amount);
     }
-} 
+
+    // (any user)
+    function transferTo(address to, uint256 amount) external {
+        _transfer(msg.sender, to, amount);
+    }
+
+    // (any user)
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
